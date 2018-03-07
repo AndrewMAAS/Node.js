dc70c9797b2f6a198751fe66178c9adbaab1d1af
admin-nv <m.aassou@ump.ac.ma>
2018-03-07 16:15:22 +0000

    my message

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,7 +5,10 @@ public class Application {
     kkkkk
         kkkkkk
         kkkkk
-
+        public static void main() {
+        
+            System.out.println("this are my changes public to the audience");
+        } 
 
 
         @Applicatio
