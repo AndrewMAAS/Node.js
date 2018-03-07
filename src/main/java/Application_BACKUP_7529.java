586de34a78ebe424adad9242b274f7ddce43ecbf
admin-nv <m.aassou@ump.ac.ma>
2018-03-07 15:03:42 +0000

    new commit

---
+++ src/main/java/Application_BACKUP_7529.java
@@ -0,0 +1,18 @@
+
+
+<<<<<<< 56360d25b30b431022a21ac02a5475cbb20da303
+public class Application() {
+
+
+    public static void main(String... args) {
+    
+=======
+
+public class Application {
+
+
+    public static void main(String ...args) {
+        System.out.println("Spring  Boot Please");
+>>>>>>> commit please
+    }
+}
