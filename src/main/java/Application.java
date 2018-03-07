1674fc6c8b612cd0e9f7fbb5b60e59a843350d46
admin-nv <m.aassou@ump.ac.ma>
2018-03-07 15:50:59 +0000

    contructor

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+
+
+public class Application {
+
+
+    @Override
+    public static void main(String... args) {
+        System.out.println("Hello World");
+        System.out.println("hghghghghghg");    
+    }
+
+    //this is andrew file
+
+}
