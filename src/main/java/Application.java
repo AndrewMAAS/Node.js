dfd148a312987967078580109f0fa8ee13ae6216
admin-nv <m.aassou@ump.ac.ma>
2018-03-08 23:48:18 +0000

    this is vs commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,70 +1,16 @@
 
-mouaad docker andrew
+
 public class Application {
 
-        public static void main() {
+    
         
-            System.out.println("this are my changes public to the audience");
-        } 
-    to do list
-    for the rest of my life
-
-
-        @Applicatio
-        
-        \
-    kkkkk
-        kkkkkk
-        kkkkk
         public static void main() {
         
             System.out.println("this are my changes public to the audience");
         } 
 
 
-        @Applicatio
-        
-        
-    kkkkk
-        kkkkkk
-        kkkkk
-        public static void main() {
-        
-            System.out.println("this are my changes public to the audience");
-        } 
 
 
-        @Applicatio
-        
-    kkkkk
-        kkkkkk
-        kkkkk
-        public static void main() {
-        
-            System.out.println("this are my changes public to the audience");
-        } 
-
-
-//
-//
-//
-//
-//
-//
-//@Applicatio
-
-
-
-
-
-
-
-        this is my file using java format in the vim editor yep
-
-
-o;iasdhdfg;kljkasdgdg
-kjhhfhfhfhe
-hgfhgfhiiriririr
-urfujrj
 
 }
