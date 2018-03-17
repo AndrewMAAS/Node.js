da9b014170e25b0757963a1881d52d9f4913f7ec
admin-nv <m.aassou@ump.ac.ma>
2018-03-17 13:31:21 +0000

    moaad spring boot application

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class Application {
+
+
+    public static void main(String ...args) {
+        System.out.println("this is my new basecode for a spring boot application");
+    }
+}
